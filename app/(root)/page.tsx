a0c4fcf1edd2b7f4dad221591a7143b629fe075f
AkashBag2004 <akash.bag@iemlabs.com>
2025-03-25 18:12:29 +0530

    sign-up setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='flex w-full items-center justify-center'>
+    Home page
+  </div>
+  )
+}
+
+export default Page
